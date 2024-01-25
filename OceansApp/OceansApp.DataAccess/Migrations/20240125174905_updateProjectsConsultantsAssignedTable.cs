905ad18c606d5a8dee2c9326c5c02ceccb760bba
Oscar Alfaro <68260919+oscaralfaguz47@users.noreply.github.com>
2024-01-25 11:51:28 -0600

    Changes in project model

---
+++ OceansApp/OceansApp.DataAccess/Migrations/20240125174905_updateProjectsConsultantsAssignedTable.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OceansApp.DataAccess.Migrations
+{
+    public partial class updateProjectsConsultantsAssignedTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "HourlyClientRate",
+                table: "PROJECTS_CONSULTANTS_ASSIGNED",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "HourlySalary",
+                table: "PROJECTS_CONSULTANTS_ASSIGNED",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsTheMontlyClientRateCalculatePerHour",
+                table: "PROJECTS_CONSULTANTS_ASSIGNED",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "MonthlyClientRate",
+                table: "PROJECTS_CONSULTANTS_ASSIGNED",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "MonthlySalary",
+                table: "PROJECTS_CONSULTANTS_ASSIGNED",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PositionDetail",
+                table: "PROJECTS_CONSULTANTS_ASSIGNED",
+                type: "nvarchar(130)",
+                maxLength: 130,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HourlyClientRate",
+                table: "PROJECTS_CONSULTANTS_ASSIGNED");
+
+            migrationBuilder.DropColumn(
+                name: "HourlySalary",
+                table: "PROJECTS_CONSULTANTS_ASSIGNED");
+
+            migrationBuilder.DropColumn(
+                name: "IsTheMontlyClientRateCalculatePerHour",
+                table: "PROJECTS_CONSULTANTS_ASSIGNED");
+
+            migrationBuilder.DropColumn(
+                name: "MonthlyClientRate",
+                table: "PROJECTS_CONSULTANTS_ASSIGNED");
+
+            migrationBuilder.DropColumn(
+                name: "MonthlySalary",
+                table: "PROJECTS_CONSULTANTS_ASSIGNED");
+
+            migrationBuilder.DropColumn(
+                name: "PositionDetail",
+                table: "PROJECTS_CONSULTANTS_ASSIGNED");
+        }
+    }
+}
