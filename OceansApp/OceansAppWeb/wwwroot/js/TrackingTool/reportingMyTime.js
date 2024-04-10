b72f05d5c4ebcb9f5d89ad615711043c69e7bf51
Oscar Alfaro <68260919+oscaralfaguz47@users.noreply.github.com>
2024-04-10 11:10:53 -0600

    Changes

--- OceansApp/OceansAppWeb/wwwroot/js/TrackingTool/reportingMyTime.js
+++ OceansApp/OceansAppWeb/wwwroot/js/TrackingTool/reportingMyTime.js
@@ -194,11 +194,7 @@ async function submitReportToBePaid() {
         const confirmation = await Swal.fire({
             title: "",
             text: `Confirm report submission? No changes are allowed afterward.`,
-            icon: null, 
-            iconHtml: '<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-alert-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12" y2="16"></line></svg>', 
-            customClass: {
-                icon: 'swal2-custom-icon' 
-            },
+            icon: 'warning',
             showCancelButton: true,
             cancelButtonText: 'Cancel',
             cancelButtonColor: '#9ba8b8',
