b2fdc47c628881232a6e9590d8aea4a26a7f1975
Oscar Alfaro <68260919+oscaralfaguz47@users.noreply.github.com>
2024-04-26 16:13:40 -0600

    Changes

--- OceansApp/OceansAppWeb/wwwroot/js/Finances/PaymentSheets/paymentSheets.js
+++ OceansApp/OceansAppWeb/wwwroot/js/Finances/PaymentSheets/paymentSheets.js
@@ -4,7 +4,6 @@ $(document).ready(function () {
     let currentDateNoChange = new Date();
     paymentPeriod = 1;
     calculatePeriod(currentDateNoChange, paymentPeriod);
-    getListOfResults(true, false);
 });
 
 // -Get list
