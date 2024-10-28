ac937521fcb02c7f0814dbbe5317f937c07fad9d
Oscar Alfaro <68260919+oscaralfaguz47@users.noreply.github.com>
2024-10-27 20:25:08 -0600

    Implementing logs

--- OceansApp/OceansAppWeb/Program.cs
+++ OceansApp/OceansAppWeb/Program.cs
@@ -19,223 +19,249 @@ using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
+var logger = LoggerFactory.Create(config =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "RIPPLE API", Version = "v1" });
-    c.CustomOperationIds(apiDesc =>
+    config.AddConsole();
+    config.AddAzureWebAppDiagnostics(); // Logs to Azure App Service
+}).CreateLogger("ProgramLogger");
+
+
+try
+{
+    // Add services to the container.
+    System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(c =>
     {
-        return apiDesc.TryGetMethodInfo(out MethodInfo methodInfo) ? methodInfo.Name : null;
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "RIPPLE API", Version = "v1" });
+        c.CustomOperationIds(apiDesc =>
+        {
+            return apiDesc.TryGetMethodInfo(out MethodInfo methodInfo) ? methodInfo.Name : null;
+        });
+        c.OperationFilter<FormDataOperationFilter>();
+        c.OperationFilter<AddAntiforgeryTokenHeaderParameter>();
     });
-    c.OperationFilter<FormDataOperationFilter>();
-    c.OperationFilter<AddAntiforgeryTokenHeaderParameter>();
-});
 
 
-builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+    builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
-// Get the current environment (Development, Production, etc.)
-var environment = builder.Environment.EnvironmentName;
+    // Get the current environment (Development, Production, etc.)
+    var environment = builder.Environment.EnvironmentName;
+    logger.LogInformation("Environment: {Environment}", environment);
 
-// App Configuration connection string for accessing secrets from Key Vault
-// Ensure the environment variable "AppConfigConnectionString" is correctly set.
-var appConfigConnectionString = Environment.GetEnvironmentVariable("AppConfigConnectionString");
+    // App Configuration connection string for accessing secrets from Key Vault
+    // Ensure the environment variable "AppConfigConnectionString" is correctly set.
+    var appConfigConnectionString = Environment.GetEnvironmentVariable("AppConfigConnectionString");
 
-if (!string.IsNullOrEmpty(appConfigConnectionString))
-{
-    // Configure Azure App Configuration with connection to Key Vault
-    builder.Configuration.AddAzureAppConfiguration(options =>
+    if (!string.IsNullOrEmpty(appConfigConnectionString))
     {
-        options.Connect(appConfigConnectionString)
-               .ConfigureKeyVault(kv =>
-               {
-                   kv.SetCredential(new DefaultAzureCredential()); // Use managed identity for authentication
-               });
+        logger.LogInformation("App Configuration connection string found. Configuring Azure App Configuration...");
+        // Configure Azure App Configuration with connection to Key Vault
+        builder.Configuration.AddAzureAppConfiguration(options =>
+        {
+            options.Connect(appConfigConnectionString)
+                   .ConfigureKeyVault(kv =>
+                   {
+                       kv.SetCredential(new DefaultAzureCredential()); // Use managed identity for authentication
+                   });
 
-        // Select all keys and configure real-time refresh
-        options.Select(KeyFilter.Any, LabelFilter.Null)
-               .ConfigureRefresh(refreshOptions =>
-               {
-                   // Use a key "AppSettings:RefreshTrigger" to trigger real-time updates
-                   refreshOptions.Register("AppSettings:RefreshTrigger", refreshAll: true)
-                                 .SetCacheExpiration(TimeSpan.FromSeconds(30));
-               });
-    });
-}
-else
-{
-    Console.WriteLine("App Configuration connection string is missing or not set in environment variables.");
-}
+            // Select all keys and configure real-time refresh
+            options.Select(KeyFilter.Any, LabelFilter.Null)
+                   .ConfigureRefresh(refreshOptions =>
+                   {
+                       // Use a key "AppSettings:RefreshTrigger" to trigger real-time updates
+                       refreshOptions.Register("AppSettings:RefreshTrigger", refreshAll: true)
+                                     .SetCacheExpiration(TimeSpan.FromSeconds(30));
+                   });
+        });
+    }
+    else
+    {
+        logger.LogWarning("App Configuration connection string is missing or not set in environment variables.");
+    }
 
-// Retrieve the database connection string depending on the environment
-string connectionString;
-environment = "test";
-if (environment == "Development") // For local environment
-{
-    // Try to get the DatabaseConnectionString from environment variables in local
-    connectionString = Environment.GetEnvironmentVariable("DefaultConnection");
+    // Retrieve the database connection string depending on the environment
+    string connectionString;
+    if (environment == "Development") // For local environment
+    {
+        // Try to get the DatabaseConnectionString from environment variables in local
+        connectionString = Environment.GetEnvironmentVariable("DefaultConnection");
 
-    // Fallback to a local connection string from appsettings if not set in environment variables
+        // Fallback to a local connection string from appsettings if not set in environment variables
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        }
+    }
+    else
+    {
+        // For Production/Demo, always use the connection string from Azure App Configuration
+        connectionString = builder.Configuration["DbConnectionString"];
+        logger.LogInformation("Using connection string from Azure App Configuration.");
+    }
+
     if (string.IsNullOrEmpty(connectionString))
     {
-        connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        logger.LogError("Database connection string could not be found.");
     }
+    else
+    {
+        logger.LogInformation("Database connection string found and applied.");
+    }
+
+    // Configure DbContext for all environments (Development, Production, and Demo)
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(connectionString));
+
+    // Register DatabaseService if needed
+    builder.Services.AddTransient<DatabaseService>(provider =>
+        new DatabaseService(connectionString));
+
+
+
+    builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+        .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+    //Configure Authorization Policies
+    AuthorizationConfig.ConfigurePolicies(builder.Services);
+
+    // Scoped services
+    builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+    builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+    builder.Services.AddScoped<ISlackRepository, SlackRepository>();
+    builder.Services.AddScoped<IAzureBlobRepository, AzureBlobRepository>();
+    builder.Services.AddScoped<IProjectConsultantAssignedHistoryRepository, ProjectConsultantAssignedHistoryRepository>();
+
+    // Lazy loading configuration for scoped services
+    builder.Services.AddScoped(typeof(Lazy<>), typeof(LazyServiceProvider<>));
+
+    // Configuring QueueClient for Azure Queue Storage
+    string queueConnectionString = builder.Configuration["AzureWebJobsStorage"];
+    builder.Services.AddSingleton(_ => new QueueClient(queueConnectionString, "emailqueue"));
+
+
+    builder.Services.Configure<IdentityOptions>(opt =>
+    {
+        opt.Password.RequiredLength = 8;
+        opt.Password.RequireLowercase = true;
+        opt.Password.RequireUppercase = true;
+        opt.Password.RequireDigit = true;
+        opt.Password.RequireNonAlphanumeric = true;
+        opt.Password.RequiredUniqueChars = 6;
+        opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+        opt.Lockout.MaxFailedAccessAttempts = 7;
+    });
+
+    builder.Services.Configure<FormOptions>(options =>
+    {
+        options.ValueCountLimit = int.MaxValue;
+        options.ValueLengthLimit = int.MaxValue;
+        options.MultipartBodyLengthLimit = long.MaxValue;
+    });
+
+    builder.Services.Configure<CookiePolicyOptions>(options =>
+    {
+        options.CheckConsentNeeded = context => false;
+        options.MinimumSameSitePolicy = SameSiteMode.None;
+    });
+
+    builder.Services.ConfigureApplicationCookie(options =>
+    {
+        options.Cookie.HttpOnly = true;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.Always; // secured cookies on production 
+        options.Cookie.SameSite = SameSiteMode.Strict;
+        options.Cookie.IsEssential = true;
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(SD.SessionExpirationTime);
+        options.SlidingExpiration = true;
+        options.AccessDeniedPath = new Microsoft.AspNetCore.Http.PathString("/Home/AccessDenied");
+        options.LoginPath = new Microsoft.AspNetCore.Http.PathString("/Account/Login");
+    });
+
+    builder.Services.AddSession(options =>
+    {
+        options.IdleTimeout = TimeSpan.FromMinutes(30);
+        options.Cookie.HttpOnly = true;
+        options.Cookie.IsEssential = true;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+    });
+    builder.WebHost.ConfigureKestrel(options =>
+    {
+        options.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(5);
+        options.Limits.RequestHeadersTimeout = TimeSpan.FromMinutes(5);
+        options.AddServerHeader = false;
+    });
+    // CORS configuration
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("AllowSpecificOrigin", builder => builder.WithOrigins("https://oceansapp.azurewebsites.net/"));
+    });
+
+    //Bonusly
+    builder.Services.AddHttpClient<BonuslyRepository>(client =>
+    {
+        client.Timeout = TimeSpan.FromSeconds(30);
+    }).ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+    {
+
+    });
+
+    // Background services
+    //builder.Services.AddHostedService<EveryOneDayServices>();
+
+    builder.Services.AddMemoryCache();
+    builder.Services.AddScoped<RequireTwoFactorEnabledAttribute>();
+    builder.Services.AddApplicationInsightsTelemetry();
+
+    logger.LogInformation("Finished configuring services.");
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RIPPLE API v1"));
+
+    app.UseHttpsRedirection();
+    app.UseRouting();
+    await SeedDatabaseAsync(app);
+    app.UseMiddleware<RedirectToDashboardMiddleware>();
+
+    app.UseSession();
+    app.UseCookiePolicy();
+    app.UseAuthentication();
+    app.UseCors("AllowSpecificOrigin");
+    app.UseAuthorization();
+
+
+    app.MapControllers();
+    app.UseStaticFiles();
+
+    app.MapRazorPages();
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllerRoute(
+            name: "areas",
+            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+        endpoints.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+    });
+
+    await app.RunAsync();
+
 }
-else
+catch (Exception ex)
 {
-    // For Production/Demo, always use the connection string from Azure App Configuration
-    connectionString = builder.Configuration["DbConnectionString"];
+    logger.LogError(ex, "An error occurred during application startup");
 }
-
-// Configure DbContext for all environments (Development, Production, and Demo)
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-
-// Register DatabaseService if needed
-builder.Services.AddTransient<DatabaseService>(provider =>
-    new DatabaseService(connectionString));
-
-
-
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
-
-//Configure Authorization Policies
-AuthorizationConfig.ConfigurePolicies(builder.Services);
-
-// Scoped services
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddScoped<IDbInitializer, DbInitializer>();
-builder.Services.AddScoped<ISlackRepository, SlackRepository>();
-builder.Services.AddScoped<IAzureBlobRepository, AzureBlobRepository>();
-builder.Services.AddScoped<IProjectConsultantAssignedHistoryRepository, ProjectConsultantAssignedHistoryRepository>();
-
-// Lazy loading configuration for scoped services
-builder.Services.AddScoped(typeof(Lazy<>), typeof(LazyServiceProvider<>));
-
-// Configuring QueueClient for Azure Queue Storage
-string queueConnectionString = builder.Configuration["AzureWebJobsStorage"];
-builder.Services.AddSingleton(_ => new QueueClient(queueConnectionString, "emailqueue"));
-
-
-builder.Services.Configure<IdentityOptions>(opt =>
-{
-    opt.Password.RequiredLength = 8;
-    opt.Password.RequireLowercase = true;
-    opt.Password.RequireUppercase = true;
-    opt.Password.RequireDigit = true;
-    opt.Password.RequireNonAlphanumeric = true;
-    opt.Password.RequiredUniqueChars = 6;
-    opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
-    opt.Lockout.MaxFailedAccessAttempts = 7;
-});
-
-builder.Services.Configure<FormOptions>(options =>
-{
-    options.ValueCountLimit = int.MaxValue;
-    options.ValueLengthLimit = int.MaxValue;
-    options.MultipartBodyLengthLimit = long.MaxValue;
-});
-
-builder.Services.Configure<CookiePolicyOptions>(options =>
-{
-    options.CheckConsentNeeded = context => false;
-    options.MinimumSameSitePolicy = SameSiteMode.None;
-});
-
-builder.Services.ConfigureApplicationCookie(options =>
-{
-    options.Cookie.HttpOnly = true;
-    options.Cookie.SecurePolicy = CookieSecurePolicy.Always; // secured cookies on production 
-    options.Cookie.SameSite = SameSiteMode.Strict;
-    options.Cookie.IsEssential = true;
-    options.ExpireTimeSpan = TimeSpan.FromMinutes(SD.SessionExpirationTime);
-    options.SlidingExpiration = true;
-    options.AccessDeniedPath = new Microsoft.AspNetCore.Http.PathString("/Home/AccessDenied");
-    options.LoginPath = new Microsoft.AspNetCore.Http.PathString("/Account/Login");
-});
-
-builder.Services.AddSession(options =>
-{
-    options.IdleTimeout = TimeSpan.FromMinutes(30);
-    options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
-    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-});
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(5);
-    options.Limits.RequestHeadersTimeout = TimeSpan.FromMinutes(5);
-    options.AddServerHeader = false;
-});
-// CORS configuration
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowSpecificOrigin", builder => builder.WithOrigins("https://oceansapp.azurewebsites.net/"));
-});
-
-//Bonusly
-builder.Services.AddHttpClient<BonuslyRepository>(client =>
-{
-    client.Timeout = TimeSpan.FromSeconds(30);
-}).ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
-{
-
-});
-
-
-// Background services
-//builder.Services.AddHostedService<EveryOneDayServices>();
-
-builder.Services.AddMemoryCache();
-builder.Services.AddScoped<RequireTwoFactorEnabledAttribute>();
-builder.Services.AddApplicationInsightsTelemetry();
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseSwagger();
-app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RIPPLE API v1"));
-
-app.UseHttpsRedirection();
-app.UseRouting();
-await SeedDatabaseAsync(app);
-app.UseMiddleware<RedirectToDashboardMiddleware>();
-
-app.UseSession();
-app.UseCookiePolicy();
-app.UseAuthentication();
-app.UseCors("AllowSpecificOrigin");
-app.UseAuthorization();
-
-
-app.MapControllers();
-app.UseStaticFiles();
-
-app.MapRazorPages();
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllerRoute(
-        name: "areas",
-        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
-
-    endpoints.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}");
-});
-
-await app.RunAsync();
 
 async Task SeedDatabaseAsync(IHost app)
 {
