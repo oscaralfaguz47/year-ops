4573957aca2992c926961ee8b6b03c3e12bfb88d
Oscar Alfaro <68260919+oscaralfaguz47@users.noreply.github.com>
2025-05-16 16:01:33 -0600

    Improvements

--- OceansApp/OceansAppWeb/wwwroot/js/Finances/DocumentsCC/createNewDocuments.js
+++ OceansApp/OceansAppWeb/wwwroot/js/Finances/DocumentsCC/createNewDocuments.js
@@ -1,16 +1,19 @@
 ﻿const searchClientElementContainer = getElementById('search-client-element');
+const headerContainerCreateDoc = document.querySelector('.create-doc-header-cont');
 const moreFormElementsContainer = getElementById('more-form-elements-container');
 const selectedClientInputCreateDoc = searchClientElementContainer.querySelector('.client-selected-input');
 const transactionTypeInputCreateDoc = document.querySelector('#transaction-Type-toogle .global-toggle-hidden-input');
 const documentNumberInputCreateDoc = getElementById('documentNumberCreateDoc');
 const clientIdInputCreateDoc = searchClientElementContainer.querySelector('#search-client-element .clientId');
 const documentTypeSelectCreateDoc = getElementById('documentTypeIdCreateDoc');
+const subtypeSelectCreateDoc = getElementById('subTypeIdCreateDoc');
 
 
 
 
 function resetToggle() {
     moreFormElementsContainer.style.display = 'none';
+    headerContainerCreateDoc.style.display = 'none';
     const transactionTypeToggleContainer = getElementById('transaction-Type-toogle');
     const debitTobbleOpt = document.querySelector('#transaction-Type-toogle .global-toggle-opt1');
     const creditTobbleOpt = document.querySelector('#transaction-Type-toogle .global-toggle-opt2');
@@ -24,7 +27,6 @@ async function displayCreateNewDocumentsModal(modalId) {
     initClientIdInputEventListener();
     selectedClientInputCreateDoc.style.display = 'none';
     clientIdInputCreateDoc.value = null;
-    await fillDocumentTypeDropdown(1);
     showModal(modalId);
 }
 
@@ -37,13 +39,20 @@ document.addEventListener('DOMContentLoaded', () => {
     if (transactionTypeInputCreateDoc) {
         transactionTypeInputCreateDoc.addEventListener('input', async (event) => {
             //execute debit or credit changes
-
             await fillDocumentTypeDropdown(transactionTypeInputCreateDoc.value);
+            if (Number(transactionTypeInputCreateDoc.value) === 2) {
+                documentNumberInputCreateDoc.value = null;
+                documentNumberInputCreateDoc.placeholder = 'Enter a Doc Number';
+                documentNumberInputCreateDoc.disabled = false;
+            } else {
+                documentNumberInputCreateDoc.disabled = true;
+                documentNumberInputCreateDoc.value = 'Select a Doc Type';
+            }
         });
     }
 });
 
-function initClientIdInputEventListener() {
+async function initClientIdInputEventListener() {
     if (!clientIdInputCreateDoc) {
         console.error("clientIdInputCreateDoc not found");
         return;
@@ -51,10 +60,13 @@ function initClientIdInputEventListener() {
 
     // Only add the observer if it has not been added before
     if (!clientIdInputCreateDoc.hasAttribute('data-observer-initialized')) {
-        const observer = new MutationObserver(() => {
-            if (clientIdInputCreateDoc.value !== '' && clientIdInputCreateDoc.value !== null) {
-                //Execute all the code after selecting the client
-
+        const observer = new MutationObserver(async () => {
+            try {
+                if (clientIdInputCreateDoc.value !== '' && clientIdInputCreateDoc.value !== null) {
+                    await handleClientSelection();
+                }
+            } catch (error) {
+                console.error("Error handling client selection:", error);
             }
         });
 
@@ -69,8 +81,25 @@ function initClientIdInputEventListener() {
 }
 
 
+async function handleClientSelection() {
+    try {
+        let debitCredit = transactionTypeInputCreateDoc.value === null ? 1 : transactionTypeInputCreateDoc.value;
+
+        await fillDocumentTypeDropdown(debitCredit);
+        moreFormElementsContainer.style.display = 'block';
+        headerContainerCreateDoc.style.display = 'flex';
+    } catch (error) {
+        console.error("Error in handleClientSelection:", error);
+    }
+}
+
 
 async function fillDocumentTypeDropdown(transactionTypeId) {
+    if (Number(transactionTypeId) === 1) {
+        documentNumberInputCreateDoc.value = 'Select a Doc Type';
+    }
+    subtypeSelectCreateDoc.disabled = true;
+    subtypeSelectCreateDoc.innerHTML = `<option>-Select a Doc Type-</option>`;
     try {
         displaySpinner();
         const documentTypesList = await getDocumentTypesListByTransactionType(transactionTypeId);
@@ -84,3 +113,9 @@ async function fillDocumentTypeDropdown(transactionTypeId) {
         hideSpinner();
     }
 }
+
+documentTypeSelectCreateDoc.addEventListener('change', async (event) => {
+    //execute debit or credit changes
+
+    subtypeSelectCreateDoc.disabled = false;
+});
